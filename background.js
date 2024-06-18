f29bb5b5174ae263ad9a3f830f4a1659c0e6820e
Yasmin <yasminsdcastro@gmail.com>
2024-06-18 17:10:45 -0300

    feat: limit to tag

--- background.js
+++ background.js
@@ -5,15 +5,41 @@ chrome.action.onClicked.addListener((tab) => {
   });
 });
 
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content.js"],
+  });
+});
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "getLastSentTime") {
-    chrome.storage.local.get(["lastSentTime"], (result) => {
-      sendResponse({ lastSentTime: result.lastSentTime });
+  if (message.action === "getGroupData") {
+    chrome.storage.local.get(["groupData"], (result) => {
+      console.log("Retrieving group data:", result.groupData);
+      sendResponse({ groupData: result.groupData || [] });
     });
     return true; // Keeps the message channel open for sendResponse
   } else if (message.action === "setLastSentTime") {
-    chrome.storage.local.set({ lastSentTime: message.time }, () => {
-      sendResponse();
+    chrome.storage.local.get(["groupData"], (result) => {
+      let groupData = result.groupData || [];
+      const groupIndex = groupData.findIndex(
+        (group) => group.groupName === message.groupName
+      );
+
+      if (groupIndex > -1) {
+        groupData[groupIndex].lastSentTime = message.time;
+      } else {
+        groupData.push({
+          groupName: message.groupName,
+          lastSentTime: message.time,
+        });
+      }
+
+      console.log("Setting group data:", groupData);
+      chrome.storage.local.set({ groupData: groupData }, () => {
+        console.log("Group data set successfully");
+        sendResponse();
+      });
     });
     return true; // Keeps the message channel open for sendResponse
   } else if (message.action === "closePopup") {
--- content.js
+++ content.js
@@ -1,20 +1,14 @@
 async function tagAllUsers() {
   try {
-    const currentTime = new Date().getTime();
-    const lastSentTime = await getLastSentTime();
-
-    console.log(`Last sent time: ${lastSentTime}`);
-    console.log(`Current time: ${currentTime}`);
-
-    if (lastSentTime && currentTime - lastSentTime < 2 * 60 * 1000) {
-      alert("Você só pode marcar novamente após 2 minutos.");
-      return;
-    }
+    const canTag = await canTagGroup();
+    if (!canTag) return;
 
     const usersFound = await getUserNames();
     await typeUsers(usersFound);
 
-    await setLastSentTime(currentTime);
+    const currentTime = new Date().getTime();
+    const groupName = await getCurrentGroupName();
+    await setLastSentTime(groupName, currentTime);
   } catch (error) {
     console.log("Unable do tag all users from group", error);
   }
@@ -59,6 +53,19 @@ async function getUserNames() {
   }
 }
 
+async function getCurrentGroupName() {
+  try {
+    const title = document.querySelector(
+      "#main > header > div._amie > div._amif > div > span"
+    );
+
+    return title.innerHTML;
+  } catch (error) {
+    console.log("Unable to get title of group", error);
+    return "";
+  }
+}
+
 async function typeUsers(usersFound) {
   try {
     const chatBox = document.querySelector(
@@ -68,8 +75,6 @@ async function typeUsers(usersFound) {
     if (chatBox) {
       for (let user of usersFound) {
         if (user === "Você") continue;
-
-        console.log(`Tagging user: ${user}`);
 
         if (user.trim().startsWith("+")) {
           user = user.slice(0, -1);
@@ -112,24 +117,23 @@ async function typeUsers(usersFound) {
   }
 }
 
-function getLastSentTime() {
+function getGroupData() {
   return new Promise((resolve) => {
-    chrome.runtime.sendMessage({ action: "getLastSentTime" }, (response) => {
+    chrome.runtime.sendMessage({ action: "getGroupData" }, (response) => {
       if (chrome.runtime.lastError) {
-        console.error("Runtime error:", chrome.runtime.lastError);
-        resolve(null);
+        resolve([]);
       } else {
-        resolve(response ? response.lastSentTime : null);
+        resolve(response ? response.groupData : []);
       }
     });
   });
 }
 
-function setLastSentTime(time) {
+function setLastSentTime(groupName, time) {
   return new Promise((resolve) => {
     chrome.runtime.sendMessage(
-      { action: "setLastSentTime", time: time },
-      () => {
+      { action: "setLastSentTime", groupName: groupName, time: time },
+      (response) => {
         if (chrome.runtime.lastError) {
           console.error("Runtime error:", chrome.runtime.lastError);
         }
@@ -137,4 +141,32 @@ function setLastSentTime(time) {
       }
     );
   });
+}
+
+async function canTagGroup() {
+  const TIME_LIMIT = 5 * 60 * 1000;
+  const currentTime = new Date().getTime();
+  const groupData = await getGroupData();
+  const groupName = await getCurrentGroupName();
+
+  const groupInfo = groupData.find((group) => group.groupName === groupName);
+
+  if (groupInfo && currentTime - groupInfo.lastSentTime < TIME_LIMIT) {
+    const timeRemaining = getTimeRemaining(
+      TIME_LIMIT - (currentTime - groupInfo.lastSentTime)
+    );
+    alert(
+      `Você só pode marcar novamente o grupo "${groupName}" após ${timeRemaining}.`
+    );
+    return false;
+  }
+
+  return true;
+}
+
+function getTimeRemaining(milliseconds) {
+  const totalSeconds = Math.floor(milliseconds / 1000);
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  return `${minutes} minutos e ${seconds} segundos`;
 }
