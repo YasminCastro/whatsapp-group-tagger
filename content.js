3459ff575237144f63e7e32c4bb401279a6b59c2
Yasmin <yasminsdcastro@gmail.com>
2024-06-18 16:40:46 -0300

    refactor: tag all users

--- content.js
+++ content.js
@@ -1,7 +1,53 @@
 async function tagAllUsers() {
   try {
     const usersFound = await getUserNames();
+    await typeUsers(usersFound);
+  } catch (error) {
+    console.log("Unable do tag all users from group", error);
+  }
+}
 
+tagAllUsers();
+
+async function getUserNames() {
+  try {
+    const groupHeader = document.querySelector('div._amie[role="button"]');
+    if (groupHeader) {
+      groupHeader.click();
+
+      await new Promise((resolve) => setTimeout(resolve, 500));
+
+      const closeButton = document.querySelector(
+        'div[role="button"][aria-label="Fechar"]'
+      );
+
+      if (closeButton) {
+        closeButton.click();
+
+        await new Promise((resolve) => setTimeout(resolve, 1500));
+
+        const userSpan = document.querySelector(
+          "div.x78zum5.x1cy8zhl.xisnujt.x1nxh6w3.xcgms0a.x16cd2qt span[title]"
+        );
+
+        if (userSpan) {
+          const userNamesText = userSpan.getAttribute("title");
+          const userNames = userNamesText.split(", ");
+
+          return userNames;
+        }
+      }
+    }
+
+    return [];
+  } catch (error) {
+    console.log("Unable to get users of group", error);
+    return [];
+  }
+}
+
+async function typeUsers(usersFound) {
+  try {
     const chatBox = document.querySelector(
       '[contenteditable="true"][data-tab="10"]'
     );
@@ -45,58 +91,10 @@ async function tagAllUsers() {
 
         await new Promise((resolve) => setTimeout(resolve, 100));
       }
-
-      const enterEvent = new KeyboardEvent("keydown", {
-        bubbles: true,
-        cancelable: true,
-        key: "Enter",
-        code: "Enter",
-        keyCode: 13,
-        charCode: 13,
-        which: 13,
-      });
-      chatBox.dispatchEvent(enterEvent);
     }
+    return;
   } catch (error) {
-    console.log("Unable do tag all users from group", error);
-  }
-}
-
-tagAllUsers();
-
-async function getUserNames() {
-  try {
-    const groupHeader = document.querySelector('div._amie[role="button"]');
-    if (groupHeader) {
-      groupHeader.click();
-
-      await new Promise((resolve) => setTimeout(resolve, 500));
-
-      const closeButton = document.querySelector(
-        'div[role="button"][aria-label="Fechar"]'
-      );
-
-      if (closeButton) {
-        closeButton.click();
-
-        await new Promise((resolve) => setTimeout(resolve, 1500));
-
-        const userSpan = document.querySelector(
-          "div.x78zum5.x1cy8zhl.xisnujt.x1nxh6w3.xcgms0a.x16cd2qt span[title]"
-        );
-
-        if (userSpan) {
-          const userNamesText = userSpan.getAttribute("title");
-          const userNames = userNamesText.split(", ");
-
-          return userNames;
-        }
-      }
-    }
-
-    return [];
-  } catch (error) {
-    console.log("Unable to get users of group", error);
-    return [];
+    console.log("Error to type users");
+    return;
   }
 }
